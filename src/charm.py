d7f1a08acbb3517096b16b48cd165b1fc4413c4d
justin <justinmauriceclark@gmail.com>
2020-08-21 11:20:57 -0400

    save data-source connection info in charm's StoredState

--- src/charm.py
+++ src/charm.py
@@ -7,7 +7,7 @@ import logging
 import setuppath  # noqa:F401
 from oci_image import OCIImageResource, OCIImageResourceError
 from ops.charm import CharmBase
-from ops.framework import StoredState
+from ops.framework import StoredState, Object
 from ops.main import main
 from ops.model import ActiveStatus, MaintenanceStatus, BlockedStatus
 
@@ -25,11 +25,9 @@ class GrafanaBase(CharmBase):
         super().__init__(*args)
 
         # get container image
+        # TODO: needs confirmation that a container works well for
+        #       non-k8s charms as well. If not, this should be removed
         self.grafana_image = OCIImageResource(self, 'grafana-image')
-
-        # used to connect to a data source
-        # TODO: should this be stored directly in application data? I still need to figure out how to best do this
-        self.grafana_source_conn = {'host': None, 'port': None}
 
         # -- hook observations that are cloud-agnostic
         self.framework.observe(self.on['grafana-source'].relation_changed,
@@ -38,34 +36,52 @@ class GrafanaBase(CharmBase):
         # -- initialize states --
         self.state.set_default(configured=False)
         self.state.set_default(started=False)
+        self.state.set_default(sources=dict())
 
     def on_http_source_available(self, event):
+        """This event handler (if the unit is the leader) will observe
+        an incoming http data source and make the host/port of the
+        source available in the app's data.
+
+        The data sources will be stored under
+        """
+
         # if this unit is the leader, set the host/port
         # of the data source in this application's data
         if not self.unit.is_leader():
             log.debug("{self.unit.name} is not leader. Cannot set app data.")
             return
 
-        # if there is no available unit, defer this event
+        # if there is no available unit, remove data-source info if it exists
         if event.unit is None:
-            log.error("event.unit cannot be None in relation_changed handler")
+            data_source = self.state.sources.pop(event.relation.id, None)
+            log.warning("removing data source information from state."
+                        "host: {0}, port: {1}.".format(
+                            data_source['host'],
+                            data_source['port'],
+                        )
+            )
+            log.error("event.unit cannot be None when setting data sources.")
             return
 
         # 'ingress-address' seems to be available in k8s and non-k8s charms
         # but it also looks like 'private-address' is also available.
         # TODO: the question is, which one should I use? Check both?
+        #       We might need to ensure data sources set these properly
+        #       And we might also want to ensure more data is passed
+        #       as is the case in the non-k8s, reactive Grafana charm
         host = event.relation.data[event.unit].get('ingress-address')
         port = event.relation.data[event.unit].get('port')
         if host is None or port is None:
-            # if host or port are None, we still want to
-            # set the grafana_source_conn to reflect this
-            log.debug("Invalid host and/or port for grafana-source relation.")
-            log.debug("Ensure 'ingress-address' and 'port' are in app data.")
+            log.debug("Invalid host and/or port for grafana-source relation.\n"
+                      "Ensure 'ingress-address' and 'port' are in app data.")
             self.model.status = BlockedStatus('Invalid data source host/port')
         else:
+            # add the new connection info to the current state
+            self.state.sources.update({event.relation.id: {
+                'host': host,
+                'port': port,
+                'rel_name': event.relation.name,
+                'rel_unit': event.unit,
+            }})
             self.model.status = ActiveStatus('Ready to connect to data source.')
-
-        # set host/port of Grafana data source in the application's data
-        # TODO: linked with above todo - should this instead be application data?
-        self.grafana_source_conn['host'] = host
-        self.grafana_source_conn['port'] = port
